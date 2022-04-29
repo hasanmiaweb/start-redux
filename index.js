7b10f0cf10b8e1ba1771fe96f6899542349349eb
hasanmiaweb <hasanmiaweb@gmail.com>
2022-04-29 14:15:54 +0600

    basic counter

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("first")
+import {createStore} from 'redux'
+
+const initialState = {
+    value: 0
+}
+
+function counterReducer( state = initialState, action) {
+    switch (action.type) {
+        case 'INCREMENT_ONE':
+            return {
+                value: state.value + 1
+            }
+        case 'DECREMENT_ONE':
+            return {
+                value: state.value - 1
+            }
+        default:
+            return state
+    }
+}
+
+let store = createStore(counterReducer)
+
+
+console.log('store', store.getState())
+store.dispatch({type:"INCREMENT_ONE"})
+console.log('store', store.getState())
+store.dispatch({type:"DECREMENT_ONE"})
+console.log('DECREMENT', store.getState())
